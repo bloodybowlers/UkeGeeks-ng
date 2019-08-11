5c0393ba8800c8b6f6a7d5faad6c42c2e4ef6a34
Louis-coding-UGNG <53580683+Louis-coding-UGNG@users.noreply.github.com>
2019-08-11 22:33:54 +0200

    Apply songlist layout to "cache rebuild" page
    Fix #52

--- ugsphp/views/songs-rebuild-cache.php
+++ ugsphp/views/songs-rebuild-cache.php
@@ -4,10 +4,13 @@
 	<meta charset="utf-8" />
 	<title>Song Index Rebuilt</title>
 	<meta http-equiv="refresh" content="4;URL=<?php echo($model->SongbooktUri) ?>" />
+	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/ugsphp.css" />
 </head>
-<body>
-	<h1><?php echo Lang::Get('cache_rebuilt');?></h1>
-	<h3><?php printf(Lang::Get('cache_rebuilt_info'), $model->ElapsedTime, $model->SongCount);?></h3>
-	<p><?php printf(Lang::Get('cache_rebuilt_redirect'), '<a href="'.$model->SongbooktUri.'">', '</a>');?></p>
+<body class="songListPage">
+	<section class="contentWrap">
+		<h1><?php echo Lang::Get('cache_rebuilt');?></h1>
+		<h3><?php printf(Lang::Get('cache_rebuilt_info'), $model->ElapsedTime, $model->SongCount);?></h3>
+		<p><?php printf(Lang::Get('cache_rebuilt_redirect'), '<a href="'.$model->SongbooktUri.'">', '</a>');?></p>
+	</section>
 </body>
 </html>
