098046d9da6a0fab2e9351befb22fcdd98092e1b
BloodyBowlers <53180097+bloodybowlers@users.noreply.github.com>
2019-08-12 08:54:35 +0000

    Added title and message border to the song cache rebuilt page

--- ugsphp/views/songs-rebuild-cache.php
+++ ugsphp/views/songs-rebuild-cache.php
@@ -2,15 +2,18 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>Song Index Rebuilt</title>
+	<title><?php echo Lang::Get('cache_rebuilt');?> | <?php echo Config::SongbookHeadline?></title>
 	<meta http-equiv="refresh" content="4;URL=<?php echo($model->SongbooktUri) ?>" />
 	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/ugsphp.css" />
 </head>
 <body class="songListPage">
 	<section class="contentWrap">
-		<h1><?php echo Lang::Get('cache_rebuilt');?></h1>
-		<h3><?php printf(Lang::Get('cache_rebuilt_info'), $model->ElapsedTime, $model->SongCount);?></h3>
-		<p><?php printf(Lang::Get('cache_rebuilt_redirect'), '<a href="'.$model->SongbooktUri.'">', '</a>');?></p>
+   <h1><?php echo Config::SongbookHeadline?></h1>
+    <div class="msgBorder">
+      <h2 style='margin-bottom:10px'><?php echo Lang::Get('cache_rebuilt');?></h2>
+      <h3><?php printf(Lang::Get('cache_rebuilt_info'), $model->ElapsedTime, $model->SongCount);?></h3>
+      <p><?php printf(Lang::Get('cache_rebuilt_redirect'), '<a href="'.$model->SongbooktUri.'">', '</a>');?></p>
+    </div>
 	</section>
 </body>
 </html>
